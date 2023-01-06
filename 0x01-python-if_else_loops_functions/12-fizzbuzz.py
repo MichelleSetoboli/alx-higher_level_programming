70144d76ee7506605f4a277462d3222e49c77480
Michellesetoboli <mesetoboli@gmail.com>
2023-01-06 06:10:42 -0800

    fix indent error

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -4,8 +4,8 @@ def fizzbuzz():
 		if num % 3 == 0 and num % 5 == 0:
 			print("FizzBuzz ", end="")
 		elif num % 5 == 0:
-	        	print("Buzz ", end="")
-	  	elif num % 3 == 0:
-	        	print("Fizz ", end="")
-	 	else:
-	 		print(f"{num} ", end="")
+			print("Buzz ", end="")
+		elif num % 3 == 0:
+			print("Fizz ", end="")
+		else:
+			print(f"{num} ", end="")
